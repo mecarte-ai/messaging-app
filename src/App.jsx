dcd137730f3b67b73023919a60856fd8cdf75d6b
mecarte-ai <mecarte.ai@gmail.com>
2023-09-03 07:17:07 +0800

    add login component

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import Login from "./Login";
+
+export default function App() {
+  return <Login />;
+}
---
+++ src/Login.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const apiURL = "http://206.189.91.54";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    width: "50%",
+  };
+
+  async function handleLogin(username, password) {
+    const res = await fetch(`${apiURL}/api/v1/auth/sign_in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password,
+      }),
+    });
+
+    let accessToken = {
+      "access-token": res.headers.get("access-token"),
+      client: res.headers.get("client"),
+      expiry: res.headers.get("expiry"),
+      uid: res.headers.get("uid"),
+    };
+
+    console.log(accessToken);
+    setIsLogin(true);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    handleLogin(username, password);
+  }
+
+  if (isLogin) {
+    return <h1>Login Successful!</h1>;
+  }
+
+  return (
+    <div className="">
+      <form action="" style={formStyle} onSubmit={handleSubmit}>
+        <label>Username: </label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Password: </label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
