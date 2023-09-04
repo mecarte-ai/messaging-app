ac080144d60eead803588aa7b47cb40c626bce47
mecarte-ai <mecarte.ai@gmail.com>
2023-09-04 21:19:14 +0800

    added previous files from main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Login from "./Login";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [isLogin, setIsLogin] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter basename="/messaging-app">
+      <Routes>
+        <Route
+          path=""
+          element={<Login onLogin={setIsLogin} isLogin={isLogin} />}
+        />
+        <Route element={<ProtectedRoute isLogin={isLogin} />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/test" element={<Test />} />
+        </Route>
+        <Route path="*" element={<h1>Page 404 not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+function Dashboard() {
+  return <h1>Welcome</h1>;
+}
+
+function Test() {
+  return <h1>Test</h1>;
+}
+
+function ProtectedRoute({ isLogin }) {
+  if (!isLogin) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
+}
